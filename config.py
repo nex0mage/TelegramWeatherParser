f3203eeb6e1b86c82c8dbb292069b6e3773b082d
Валерий Блохин <tracer3228@mail.ru>
2022-03-26 18:32:43 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,2 @@
+open_weather_token = "02c0c2665d4e05e5be06710edc648a19"
+telegram_bot_token = "5269869906:AAGyLq9luoksBu-GfAdpehBrm7G6btFYa9k"
---
+++ telegram_bot_core.py
@@ -0,0 +1,65 @@
+import requests
+import datetime
+from config import telegram_bot_token, open_weather_token
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+bot = Bot(token=telegram_bot_token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    await message.reply("Пивет! Напиши название города,мне.. чтобы посмотреть погодку.")
+
+
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    code_to_smile = {
+        "Clear": "Ясно! Солнышко!!! \U00002600",
+        "Clouds": "Облакаа \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождичек \U00002614",
+        "Thunderstorm": "Гроза тыдыщбахбах!! \U000026A1",
+        "Snow": "Снежочек \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "А ты сам видел что за окном?.."
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+
+        await message.reply(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+              f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+              f"удачки <3"
+              )
+
+    except:
+        await message.reply("что это за город? ты его выдумал если, то так и скажи.. ")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
